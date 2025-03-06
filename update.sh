7fe3d0a5a766f972f5d1d051d6febffc6c7d87cc
Ian <46545548+ian-craig0@users.noreply.github.com>
2025-03-06 14:41:48 -0600

    Update update.sh
    added python script updating

--- update.sh
+++ update.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 sudo systemctl stop kiosk.service
 
+REPO_URL="https://github.com/ian-craig0/Scanny-Project.git"
+REPO_DIR="Scanny-Project"  # Local clone directory
+TARGET_DIR="/home/pi/Desktop/scanny"  # Case-sensitive path!
+
+# Clone or update the repository
+if [ -d "$REPO_DIR" ]; then
+  git -C "$REPO_DIR" pull
+else
+  git clone "$REPO_URL" "$REPO_DIR"
+fi
+
+# Update or create the target directory
+if [ -d "$TARGET_DIR" ]; then
+  rsync -a --delete "$REPO_DIR/scanny/" "$TARGET_DIR/"
+else
+  cp -r "$REPO_DIR/scanny" "$TARGET_DIR"
+fi
+
+# Fix permissions (since script runs with sudo)
+chown -R pi:pi "$TARGET_DIR"
+
 sudo systemctl start kiosk.service
