a2515332deaa4614f18d4118f9e709e11cf6fe9a
Ian <46545548+ian-craig0@users.noreply.github.com>
2025-03-26 09:41:24 -0500

    Update main.py

--- scanny/main.py
+++ scanny/main.py
@@ -245,6 +245,7 @@ def refresh_teacher_frame():
 def close_success_scan():
     time.sleep(2)
     window.after(0, lambda: successFrame.lower())
+    window.after(1, tabSwap, 1)
 
 #CHANGING DATA FUNCTIONS
 def tempResetArrivalTimes():
@@ -470,7 +471,6 @@ def checkIN():
                                                 callMultiple(checkInCursor, """INSERT INTO scans (period_ID, schedule_ID, macID, scan_date, scan_time, status, reason) values (%s, %s, %s, %s, %s, %s, %s)""", (period_ID, get_active_schedule_ID(), ID, scan_date, scan_time, status, None), False, False)
                                                 window.after(0, lambda i0 = scan_time, i1 = ID, i2 = status: successScan(i0, i1, i2))
                                                 #window.after(0, lambda i0 = period_ID: studentListPop(i0))
-                                                window.after(0, tabSwap, 1)
                                         else: #IF ONE OF THEIR PERIODS IS not MATCHING WITH THE CURRENT PERIOD
                                             continue
                                     if notInPeriod:
